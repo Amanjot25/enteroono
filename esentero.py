cf4122d267fa0cd70b9e398d540add5fdaa0ac03
ism23819737 <ism23819737@a24.informatica.escoladeltreball.org>
2018-01-25 12:54:34 +0100

    entero

---
+++ esentero.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+dividendo = int(input("Dime el dividendo:"))
+divisor = int(input("Dime el divisor:"))
+
+if dividendo % divisor:
+	print "La división no es exacta"
+else:
+	print "La división es exacta"
